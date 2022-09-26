c159064ee84b130b8b348d3c1bc6163795b9d459
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2022-09-26 15:33:19 -0500

    New weather API

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <h1 className="text-3xl font-bold underline"> Tulancingo </h1>
   )
 }
